9e97334813b46c3b88a1e190a97e1a0ee9ef2971
Arjun Nair <nair.arjun47@yahoo.com>
2020-06-17 22:35:48 -0700

    Made time check. Overly more complex than the answer, but it works.

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,3 +1,6 @@
+import java.time.LocalDateTime
+import java.util.*
+
 fun main(args: Array<String>) {
     println("Hello, ${args[0]}!")
 
@@ -10,4 +13,15 @@ fun main(args: Array<String>) {
 
     val message = "You are ${ if (temperature > 50) "fried" else "safe" } fish"
     println(message)
+
+    val current = LocalDateTime.now()
+    val cal = Calendar.getInstance()
+    val hours = cal.get(Calendar.HOUR_OF_DAY)
+
+    val greetings = "${ if (hours < 12) "Good morning, ${args[0]}" else "Good night, ${args[0]}" }"
+
+    println("Current Date and Time is: $current")
+    println("Current Hour is: $hours")
+    println("$greetings")
+
 }
