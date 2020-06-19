b167f7b758aea32f9d8fc421d131cd0fdeb19185
Arjun Nair <nair.arjun47@yahoo.com>
2020-06-18 23:30:31 -0700

    Add to WhatToDO. Improve it.

--- src/WhatToDO.kt
+++ src/WhatToDO.kt
@@ -1,13 +1,23 @@
 fun main(args: Array<String>) {
-    whatShouldIDoToday("sad")
+    println(whatShouldIDoToday("sad"))
+    println(whatShouldIDoToday("happy", "sunny"))
+    print("How do you feel?")
+    println(whatShouldIDoToday(readLine()!!))
 }
+
+fun isVeryHot (temperature: Int) = temperature > 35
+fun isSadRainyCold (mood: String, weather: String, temperature: Int) =
+        mood == "sad" && weather == "rainy" && temperature == 0
+fun isHappySunny (mood: String, weather: String) = mood == "happy" && weather == "sunny"
 
 fun whatShouldIDoToday(
         mood : String,
         weather : String = "sunny",
         temperature : Int = 24 ) : String {
     return when {
-        mood == "happy" && weather == "Sunny" -> "go for a walk"
+        isVeryHot(temperature) -> "go swimming"
+        isSadRainyCold(mood, weather, temperature) -> "stay in bed"
+        isHappySunny(mood, weather) -> "go for a walk"
         else -> "Stay home and read."
     }
 
